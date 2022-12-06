8e5e353e0c90951a378500a618ebab90febfe32c
jorgepasco1 <jorgepascosoto@gmail.com>
2022-12-06 02:09:34 -0500

    add day 6

---
+++ 06-tuning-trouble/main.py
@@ -0,0 +1,41 @@
+def read_input():
+    with open('input', 'r') as buffer:
+        datastream = buffer.read()
+    return datastream
+
+
+def all_different(string: str) -> bool:
+    return len(set(string)) == len(string)
+
+
+def find_unique_sequence_position(string: str, amount_: int) -> int:
+    for idx in range(len(string)):
+        if idx < amount_:
+            continue
+
+        if all_different(string[idx - amount_: idx]):
+            return idx
+    return 0
+
+
+def find_start_of_packet(buffer: str) -> int:
+    return find_unique_sequence_position(buffer, 4)
+
+
+def find_start_of_message(buffer: str) -> int:
+    return find_unique_sequence_position(buffer, 14)
+
+
+def part_one() -> None:
+    datastream = read_input()
+    print(find_start_of_packet(datastream))
+
+
+def part_two() -> None:
+    datastream = read_input()
+    print(find_start_of_message(datastream))
+
+
+if __name__ == "__main__":
+    part_one()
+    part_two()
